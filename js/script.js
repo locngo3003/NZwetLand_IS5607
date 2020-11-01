d0797e9389111df66e028773fc1f338c17dbf447
locngo3003 <38222089+locngo3003@users.noreply.github.com>
2020-11-01 23:36:31 +1300

    updated

--- js/script.js
+++ js/script.js
@@ -79,6 +79,26 @@ function getSingleanimal(animalID){
     });
     return animal1;
 }
+function getSingleObservation(observationID){
+    //return case object
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async:false,
+        url: 'https://nzwetland.herokuapp.com/api/Observation/'+observationID+'/',
+        success: function (data) {
+            // location.reload();
+            observations = data;
+
+        },
+        error: function (err) {
+            console.log(err);
+        }
+
+    });
+    return observations;
+}
+
 function recordedObservation(siteHabitat){
     //return assessment objects
     recordedObservations = [];
@@ -105,4 +125,87 @@ function recordedObservation(siteHabitat){
 
     });
     return recordedObservations;
+}
+
+
+
+
+
+
+
+
+BaseURL = "https://nzwetland.herokuapp.com/";
+
+function login(username, password){
+    $.ajax({
+        type: 'POST',
+        dataType: 'JSON',
+        url: BaseURL+'api/auth/login',
+        data: {username : username, password: password},
+        async: false,
+        success: function (data) {
+            localStorage.setItem('user', JSON.stringify(data.user));
+            localStorage.setItem('token', data.token);
+            console.log(JSON.stringify(data.user));
+            console.log(data.token);
+            location.reload();
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
+
+function hasValidToken(){
+    token = localStorage.getItem('token');
+    var loginStatus = false;
+    $.ajax({
+        type: 'GET',
+        dataType: 'JSON',
+        url: BaseURL+'api/auth/user',
+        headers: { 'Authorization': 'Token ' + token },
+        async: false,
+        success: function (data) {
+            console.log(data);
+            loginStatus = true;
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    return loginStatus;
+}
+
+function hasValidLogin(){
+    var loginStatus = false;
+    try {
+        user = JSON.parse(localStorage.getItem('user'));
+        token = localStorage.getItem('token');
+        // console.log("validToken: "+hasValidToken());
+        if (hasValidToken()){
+            loginStatus = true;
+        }
+    }
+    catch(err) {
+        console.log("no user login");
+    }
+    return loginStatus;
+}
+
+function logout(){
+    $.ajax({
+        type: 'POST',
+        dataType: 'JSON',
+        url: BaseURL+'api/auth/logout',
+        headers: { 'Authorization': 'Token ' + token },
+        async: false,
+        success: function (data) {
+            console.log(data);
+            localStorage.clear();
+            location.reload();
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
 }
